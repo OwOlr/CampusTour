75eebbb9b23cba6ef4d41ce22839128b0e718719
OwOlr <dbfl8154@gmail.com>
2023-06-07 03:15:44 +0900

    상점뷰 레이아웃 셋팅완료
    상점뷰 레이아웃 셋팅완료
    코드작성 할 예정

--- Assets/JUR/Scripts/Kiosk/StoreMenuButton.cs
+++ Assets/JUR/Scripts/Kiosk/StoreMenuButton.cs
@@ -13,21 +13,31 @@ public class StoreMenuButton : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI[] btnTxt;
 
+    [SerializeField]
+    private string[] str_btnTxts;
+   
+    private string hexOncolor = "#000000";
+    private string hexOffcolor = "#AACE37";
+
+
+
 
 
     private void Start()
     {
         menuBtns = this.GetComponentsInChildren<Button>();
         btnTxt = new TextMeshProUGUI[menuBtns.Length];
+        str_btnTxts = new string[menuBtns.Length];
+
 
         for (int i = 0; i < menuBtns.Length; i++)
         {
-
             btnTxt[i] = menuBtns[i].GetComponentInChildren<TextMeshProUGUI>();
-
+            str_btnTxts[i] = btnTxt[i].text;
         }
 
-        btnTxt[0].text = "<color=#AACE37>카메라</color>";
+        btnTxt[0].text = "<color="+ hexOncolor + ">" + str_btnTxts[0] + "</color>";
+        menuBtns[0].image.color = new Color32(255, 255, 255, 255);
 
     }
 
@@ -37,21 +47,24 @@ public class StoreMenuButton : MonoBehaviour
     {
         for (int i = 0; i < menuBtns.Length; i++)
         {
-            btnTxt[i].color = new Color(170, 206, 55);
-
+            btnTxt[i].text = "<color=" + hexOffcolor + ">" + str_btnTxts[i] + "</color>";
+            menuBtns[i].image.color = new Color32(101, 46, 141, 255);
         }
-        btnTxt[0].text = "<color=#AACE37>카메라</color>";
+
+        btnTxt[0].text = "<color=" + hexOncolor + ">" + str_btnTxts[0] + "</color>";
+        menuBtns[0].image.color = new Color32(255, 255, 255, 255);
+
 
     }
     public void OnStoreProductMenu()
     {
         for (int i = 0; i < menuBtns.Length; i++)
         {
-            btnTxt[i].color = new Color(170, 206, 55);
-
+            btnTxt[i].text = "<color=" + hexOffcolor + ">" + str_btnTxts[i] + "</color>";
+            menuBtns[i].image.color = new Color32(101, 46, 141, 255);
         }
-        btnTxt[1].color = new Color(0, 0, 0);
-
+        btnTxt[1].text = "<color=" + hexOncolor + ">" + str_btnTxts[1] + "</color>";
+        menuBtns[1].image.color = new Color32(255, 255, 255, 255);
     }
 
 
