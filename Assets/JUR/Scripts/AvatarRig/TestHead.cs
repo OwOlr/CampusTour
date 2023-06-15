93dcabcdc5e05546715dffa26b6e645e21bbb4ff
OwOlr <dbfl8154@gmail.com>
2023-06-15 17:02:15 +0900

    캐릭터리깅 완료
    1.캐릭터 상의 리깅 완료
    2. 나무 리터칭 완료
    
    추가사항 : 캐릭터 움직임 애니메이션 추가
    캠퍼스 내 나무 리터칭 완료하기
    ui정리하기

--- Assets/JUR/Scripts/AvatarRig/TestHead.cs
+++ Assets/JUR/Scripts/AvatarRig/TestHead.cs
@@ -6,10 +6,11 @@ public class TestHead : MonoBehaviour
 {
     public Transform controller;
 
+    public Vector3 positionOffset;
     public Vector3 rotationOffset;
     private void LateUpdate()
     {
-        this.transform.position = controller.transform.position;
+        this.transform.position = controller.transform.position + positionOffset;
         this.transform.rotation = controller.transform.rotation * Quaternion.Euler(rotationOffset);
     }
 }
